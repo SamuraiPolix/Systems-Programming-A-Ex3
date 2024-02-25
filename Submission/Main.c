a021e18f2d6141a5bf2e603a817b2a9a175b0bf5
SamuraiPolix <sam.lazareanu@gmail.com>
2024-02-25 15:43:01 +0200

    finish main

--- Submission/Main.c
+++ Submission/Main.c
@@ -1,14 +1,15 @@
-
+#include "StrList.h"
+#define MAX_CHARS 2047
 #define INSERT_MANY_TO_LIST 1
 #define INSERT_TO_SPECIFIC_INDEX 2
 #define PRINT_LIST 3
-#define PRINT_LIST_LENGTH 4
+#define PRINT_LIST_SIZE 4
 #define PRINT_SPECIFIC_INDEX 5
 #define PRINT_NUM_OF_CHARS 6
 #define HOW_MANY_TIMES_STR_IN_LIST 7
 #define REMOVE_STR_FROM_LIST 8
 #define REMOVE_SPECIFIC_INDEX 9
-#define INVERT_LIST 10
+#define REVERSE_LIST 10
 #define DELETE_LIST 11
 #define SORT_LEXI 12
 #define CHECK_SORTED_LEXI 13
@@ -36,32 +37,88 @@
  
 int main() {
     int action;
+    int numOfInputs, index;
+    char word[2048];
+
+    StrList* strList = StrList_alloc();
+
+    if (strList == NULL){
+        printf("%s\n", MEMORY_ALLOCATION_ERROR);
+        return -1;
+    }
+
 
     do {
         scanf("%d", &action);
+
+        while ((getchar()) != '\n');       // Clears the input buffer
         
         switch (action)
         {
             case INSERT_MANY_TO_LIST:
-                
+                scanf("%d", &numOfInputs);
+                getchar();
+                for (int i = 0; i < numOfInputs; i++){
+                    scanf("%s ", word);
+                    StrList_insertLast(strList, word);
+                }
+                break;
             case INSERT_TO_SPECIFIC_INDEX:
+                scanf("%d", &index);
+                getchar();
+                fgets(word, MAX_CHARS, stdin);
+                int len = strlen(word);
+                // remove line break if exists
+                if (len > 0 && word[len-1] == '\n'){
+                    word[len-1] = 0;
+                }
+                StrList_insertAt(strList, word, index); 
+                break;
             case PRINT_LIST:
-            case PRINT_LIST_LENGTH:
+                StrList_print(strList);
+                break;
+            case PRINT_LIST_SIZE:
+                printf("%ld\n", StrList_size(strList));
+                break;
             case PRINT_SPECIFIC_INDEX:
+                scanf("%d", &index);
+                StrList_printAt(strList, index);
+                break;
             case PRINT_NUM_OF_CHARS:
+                printf("%d\n", StrList_printLen(strList));
+                break;
             case HOW_MANY_TIMES_STR_IN_LIST:
+                fgets(word, MAX_CHARS, stdin);
+                printf("%d\n", StrList_count(strList, word));
+                break;
             case REMOVE_STR_FROM_LIST:
+                fgets(word, MAX_CHARS, stdin);
+                StrList_remove(strList, word);
+                break;
             case REMOVE_SPECIFIC_INDEX:
-            case INVERT_LIST:
+                scanf("%d", &index);
+                StrList_removeAt(strList, index);
+                break;
+            case REVERSE_LIST:
+                StrList_reverse(strList);
+                break;
             case DELETE_LIST:
+                StrList_free(strList);
+                break;
             case SORT_LEXI:
+                StrList_sort(strList);
+                break;
             case CHECK_SORTED_LEXI:
+                printf("%s\n", StrList_isSorted(strList) == 0 ? "false" : "true");
+                break;
+            case EOF:
             case EXIT:
                 return 0;
                 break;        
             default:
-                print("ERROR: incorrect input!");
+                printf("ERROR: incorrect input!");
         }
     } while (action != 0);
 
+    return 0;
 }
